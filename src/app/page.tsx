297696eb157692350ad30bf671ca1b4c0ec8e4cc
OlgitH <berman.oj@gmail.com>
2026-04-21 10:01:08 +0100

    typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="h-screen w-screen font-sans bg-black text-white">
       <main className="flex flex-1 w-full h-full items-center justify-center py-32 px-16 text-center">
         <section className="">
-          <h1>Green Crown Studio</h1>
+          <h1>Greencrown Studio</h1>
           <h2>Coming soon</h2>
         </section>
       </main>
