d1b5053fe7dad86b94de8870cd4e4788283f4416
OlgitH <berman.oj@gmail.com>
2026-05-02 11:08:46 +0100

    fix typo

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -10,7 +10,7 @@ export default function AboutPage() {
 				<p className="mb-8 max-w-2xl font-light">
 					Greencrown Studio is can be your personalised creative department. We like to work with small to medium companies, or individuals, from all industries - including artists, architects, engineering and manufacturing.  We can help you simplify your tech and make it work for you, instead of bringing noise and headaches. 
 
-                    <Link href="https://www.oliverberman.net" className="underline" target="_blank" rel="noopener noreferrer">Oliver Berman</Link> is the founder of Greencrown Studio. He has a background web development and design, working mainly in universities. Whilst designing and building websites and brands is part of what we do, what excites us most is visualising and bringing meaning to data, and working out ways to make it more engaging and accessible. 
+                    <Link href="https://www.oliverberman.net" className="underline" target="_blank" rel="noopener noreferrer">Oliver Berman</Link> is the founder of Greencrown Studio. He has a background in web development and design, working mainly in universities. Whilst designing and building websites and brands is part of what we do, what excites us most is visualising and bringing meaning to data, and working out ways to make it more engaging and accessible. 
 				</p>
 
 			</main>
