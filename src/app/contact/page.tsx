7a43486be59e4d56d14dccf9dc536bc63cb601e5
OlgitH <berman.oj@gmail.com>
2026-09-08 09:17:17 +0100

    remove image contact

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import BackHomeLink from "../components/BackHomeLink";
 import PageMain from "../components/PageMain";
 
@@ -8,14 +7,7 @@ export default function ContactPage() {
 			<BackHomeLink />
 			<PageMain>
 				<section className="relative max-w-4xl font-light">
-					<Image
-						src="/hummingbird.png"
-						alt=""
-						width={900}
-						height={1200}
-						aria-hidden="true"
-						className="absolute -top-10 -left-10 z-0 w-[280px] h-auto object-contain opacity-40 pointer-events-none origin-top-left md:w-[500px]"
-					/>
+					
 					<div className="relative z-10 max-w-2xl">
 					<h1 className="text-3xl font-bold mb-4">Contact</h1>
 					<p className="mb-4">
